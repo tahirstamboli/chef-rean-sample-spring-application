d73294cb19cad8c753cad7438a22618be78f88f1
tahirstamboli <tahirstamboli@hotmail.com>
2019-09-20 18:32:11 +0530

    hypen replaced by underscore

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,22 +4,22 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-download-path = default['chef-rean-sample-spring-application']['download-path']
-db-user = default['chef-rean-sample-spring-application']['db-user']
-db-password = default['chef-rean-sample-spring-application']['db-password']
-db-name = default['chef-rean-sample-spring-application']['db-name']
+download_path = default['chef-rean-sample-spring-application']['download_path']
+db_user = default['chef-rean-sample-spring-application']['db_user']
+db_password = default['chef-rean-sample-spring-application']['db_password']
+db_name = default['chef-rean-sample-spring-application']['db_name']
 port = default['chef-rean-sample-spring-application']['port']
-application-context = default['chef-rean-sample-spring-application']['application-context']
+application_context = default['chef-rean-sample-spring-application']['application_context']
 host = default['chef-rean-sample-spring-application']['host']
 
 bash 'Install Sample Java Based Spring Application' do
     user 'root'
     cwd '/tmp'
     code <<-EOH
-    wget #{download-path}
-    nohup java -jar -DPORT=#{port} -DDB_NAME=#{db-name} -DDB_USER=#{db-user} -DDB_PWD=#{db-password} sampleapplication-2.0.0.jar &
+    wget #{download_path}
+    nohup java -jar -DPORT=#{port} -DDB_NAME=#{db_name} -DDB_USER=#{db_user} -DDB_PWD=#{db_password} sampleapplication-2.0.0.jar &
     firewall-cmd --add-port=#{port}/tcp --permanent
     firewall-cmd --reload
-    echo "http://#{host}:#{port}/#{application-context}"
+    echo "http://#{host}:#{port}/#{application_context}"
     EOH
 end
