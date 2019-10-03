40d91e98834ba2c9eeef35cbc8eba3dc780f7fbf
tahirstamboli <tahirstamboli@hotmail.com>
2019-10-03 23:06:03 +0530

    enable firewall port attribute is added.

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,7 @@ db_name = node['chef-rean-sample-spring-application']['db_name']
 port = node['chef-rean-sample-spring-application']['port']
 application_context = node['chef-rean-sample-spring-application']['application_context']
 host = node['chef-rean-sample-spring-application']['host']
+enable_firewall_port = node['chef-rean-sample-spring-application']['enable_firewall_port']
 
 bash 'Install Sample Java Based Spring Application' do
     user 'root'
@@ -18,9 +19,18 @@ bash 'Install Sample Java Based Spring Application' do
     code <<-EOH
     wget #{download_path}
     nohup java -jar -DPORT=#{port} -DDB_NAME=#{db_name} -DDB_USER=#{db_user} -DDB_PWD=#{db_password} sampleapplication-2.0.0.jar &
-    firewall-cmd --add-port=#{port}/tcp --permanent
-    firewall-cmd --reload
     EOH
+end
+
+unless enable_firewall_port.empty?
+    bash 'Open Firewall Port for Application' do
+        user 'root'
+        cwd '/tmp'
+        code <<-EOH
+        firewall-cmd --add-port=#{port}/tcp --permanent
+        firewall-cmd --reload
+        EOH
+    end
 end
 
 puts "============================================="
